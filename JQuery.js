e88bec5ca2c1adab4d748111e82a003e3d445592
nilnasalsabila484 <nilnasalsabila484@gmail.com>
2025-12-09 15:03:04 +0700

    inisialisasi projek

---
+++ JQuery.js
@@ -0,0 +1,14 @@
+<div class="container my-s">
+    <div class="row g-4">
+        <div class="col-md-4">
+            <div class="card"></div>
+            <img src="produk1.jpg" class="card-img-top" alt="produk 1"></img>
+            <div class="card-body"></div>
+             <h5 class="card-title">
+                <p class="card-text"> deskripsi singkat produk.</p>
+                 <a href="#" class="btn btn-primary"> beli sekarang</a>
+             </h5>
+        </div>
+    </div>
+</div>
+                      
